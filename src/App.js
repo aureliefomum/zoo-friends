92d1ce6634c10c7fc7f12f9ae941ef38ce918f8e
aureliefomum <azam_aurelie@yahoo.com>
2022-03-23 21:59:03 -0300

    created dropdown select tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import ProductDropDown from "./components/ProductDropdown";
+import StatesDropDown from "./components/StatesDropdown";
+import CitiesDropDown from "./components/CitiesDropdown";
 
 const API_URL = "https://assessment-edvora.herokuapp.com/";
 
@@ -16,7 +19,30 @@ const App = () => {
 
 	return (
 		<div className="container">
-			<div className="sidebar"></div>
+			<div className="sidebar">
+				<div className="filterHeading">Filters</div>
+				<div className="products-dropdown-container">
+					<label htmlFor="products"></label>
+					<select name="products" className="allSelect">
+						<option value="">Products</option>
+					</select>
+				</div>
+
+				<div className="states-dropdown-container">
+					<label htmlFor="states"></label>
+					<select name="states" className="allSelect">
+						<option value="">States</option>
+					</select>
+				</div>
+
+				<div className="cities-dropdown-container">
+					<label htmlFor="cities"></label>
+					<select name="cities" className="allSelect">
+						<option value="">Cities</option>
+					</select>
+				</div>
+			</div>
+
 			<div className="caroussel-section"></div>
 		</div>
 	);
