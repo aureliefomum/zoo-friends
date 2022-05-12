c7a5b4710861662143184d9ae39b5a3ef13dc94a
aureliefomum <azam_aurelie@yahoo.com>
2022-05-12 18:33:51 -0300

    added active time filter

--- src/App.jsx
+++ src/App.jsx
@@ -44,17 +44,70 @@ function App() {
     // set the state of allTypes
     setAllTypes([...uniqueTypes]);
 
-    // create a variable to hold all the states
+    // create a variable to hold all the animals
     const times = results.data.map((animal) => animal.active_time);
     const uniqueTimes = new Set(times);
 
-    // set the state of the allStates to the new unique array
+    // set the state of the allAnimals to the new unique array
     setAllActiveTimes([...uniqueTimes]);
   }
 
   useEffect(() => {
     getData();
   }, []);
+
+  const defaultCards = allResults.map(() => (
+    <Card
+      type="Bird"
+      name="Boat-Billed Heron"
+      image="https://upload.wikimedia.org/wikipedia/commons/c/c6/Tyto_alba_-British_Wildlife_Centre%2C_Surrey%2C_England-8a_%281%29.jpg"
+      latinName="Cochlearius cochlearius"
+      lifeSpan="24years"
+      habitat="Dry and tropical"
+      diet
+    />
+  ));
+
+  // animal =>
+  // do I have a filter set? no, return true (give me back every item)
+  // otherwise, should I return this item?
+  // animaltype == myfilter? true (gives you it back), false (filters it out)
+  const filteredCards = allResults
+    .filter((animal) => {
+      if (selectedType === "") {
+        return true;
+      }
+      return animal.animal_type.toLowerCase() === selectedType.toLowerCase();
+    })
+    .filter((animal) => {
+      if (selectedActiveTime !== "") {
+        return true;
+      }
+      return (
+        animal.active_time.toLowerCase() === selectedActiveTime.toLowerCase()
+      );
+    })
+    .map((animal) => (
+      <Card
+        type={animal.animal_type}
+        name={animal.name}
+        image={animal.image_link}
+        latinName={animal.latin_name}
+        lifeSpan={animal.lifespan}
+        habitat={animal.habitat}
+        diet={animal.diet}
+      />
+    ));
+
+  const isFilterPresent = selectedType !== "" || selectedActiveTime !== "";
+  const filtersStrings = [selectedType, selectedActiveTime]
+    .map((item) => {
+      if (item !== "") {
+        return item;
+      }
+      return null;
+    })
+    .filter((item) => item !== null);
 
   return (
     <div className="container">
@@ -83,7 +136,7 @@ function App() {
 
       <div className="main-section">
         <h1>Zoo Friends</h1>
-        <h2>All Animals</h2>
+        <h3 className="animalsHeading">All Animals</h3>
 
         <div className="caroussel-container">
           <Carousel>
@@ -101,26 +154,9 @@ function App() {
           </Carousel>
         </div>
         <h3>
-          Animals by Type: <span />
-          {selectedType}
+          {!isFilterPresent ? "Choose a Filter" : `Filters: ${filtersStrings}`}
         </h3>
-        {/* <div className='cards-area'></div>
-         */}
-        <Carousel>
-          {allResults
-            .filter((animal) => animal.animal_type === selectedType)
-            .map((animal) => (
-              <Card
-                type={animal.animal_type}
-                name={animal.name}
-                image={animal.image_link}
-                latinName={animal.latin_name}
-                lifeSpan={animal.lifespan}
-                habitat={animal.habitat}
-                diet={animal.diet}
-              />
-            ))}
-        </Carousel>
+        <Carousel>{!isFilterPresent ? defaultCards : filteredCards}</Carousel>
 
         {/* <div>
 					<h3>{title}</h3>
