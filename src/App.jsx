a17f967bfaecc459811eb5d026090824c507ced5
Aurelie Fomum <azam_aurelie@yahoo.com>
2022-09-01 16:45:00 -0300

    return statements added

--- src/App.jsx
+++ src/App.jsx
@@ -12,53 +12,35 @@ const API_URL = "https://zoo-animal-api.herokuapp.com/animals/rand/10";
 
 function App() {
   const [allResults, setAllResults] = useState([]);
-  //   const [allAnimals, setAllAnimals] = useState([]);
-  // const [allLifeSpans, setAllLifeSpans] = useState({ages1to20:[], ages20to30:[], ages30to50:[]});
   const [allTypes, setAllTypes] = useState([]);
   const [selectedLifeSpan, setSelectedLifeSpan] = useState("");
   const [allActiveTimes, setAllActiveTimes] = useState([]);
-  // const [selectedAnimal, setSelectedAnimal] = useState("");
   const [selectedType, setSelectedType] = useState("");
   const [selectedActiveTime, setSelectedActiveTime] = useState("");
 
   async function getData() {
     const results = await axios.get(API_URL);
-    // console.log(results);
 
-    // create a variable to hold the array of animals
     const animalObjects = results.data;
 
-    // set 'all animals' State to new array
     setAllResults(animalObjects);
-
-    // create a variable to hold the animal names
-    // map through the animal names and create a new array of strings
-    // (set this new array to the variable you created)
-    //const animals = results.data.map((animal) => animal.name);
 
     const lifeSpans = results.data.map((animal) => animal.lifespan);
     console.log("all life spans:", lifeSpans);
 
     const uniqueLifeSpans = new Set(lifeSpans);
-    // setAllLifeSpans([...uniqueLifeSpans]);
+
     console.log("unique life spans", uniqueLifeSpans);
 
-    // set 'all animals names' State to new array
-    //setAllAnimals(animals);
-
-    // create a variable to hold all the states
     const types = results.data.map((animal) => animal.animal_type);
 
     const uniqueTypes = new Set(types);
 
-    // set the state of allTypes
     setAllTypes([...uniqueTypes]);
 
-    // create a variable to hold all the animals
     const times = results.data.map((animal) => animal.active_time);
     const uniqueTimes = new Set(times);
 
-    // set the state of the allAnimals to the new unique array
     setAllActiveTimes([...uniqueTimes]);
   }
 
@@ -79,87 +61,24 @@ function App() {
     />
   ));
 
-  //    const lifeSpansBelow20 = allLifeSpans.filter((lifespan) => lifespan < 20)
-  //    console.log('filtered life spans below 20', lifeSpansBelow20)
-
-  //    const lifeSpansBelow30 = allLifeSpans.filter((lifespan) => lifespan > 20 && lifespan < 30)
-  //    console.log('filtered life spans below 30', lifeSpansBelow30)
-
-  //    const lifeSpansBelow50 = allLifeSpans.filter((lifespan) => lifespan > 30 && lifespan < 50)
-  //    console.log('filtered life spans below 50', lifeSpansBelow50)
-
-  //    setAllLifeSpans({
-  //     ages1to20:lifeSpansBelow20, ages20to30:lifeSpansBelow30, ages30to50:lifeSpansBelow50
-  //    })
-
-  // create a variable to hold the lifespan options you want to display in the dropdown
   const lifespanOptions = ["1-20 years", "21-30 years", "31-50 years"];
-  //   const filteredLifeSpans = allResults
-  //     .filter((animal) => {
-  //       if (selectedLifeSpan === "") {
-  //         return true;
-  //       }
-  //       if (selectedLifeSpan === "1-20 years") {
-  //         if (animal.lifespan < 20) {
-  //           return true;
-  //         }
-  //       }
-  //       if (selectedLifeSpan === "21-30 years") {
-  //         if (animal.lifespan > 20 && animal.lifespan < 30) {
-  //           return true;
-  //         }
-  //       }
-  //       if (selectedLifeSpan === "31-50 years") {
-  //         if (animal.lifespan > 30) {
-  //           return true;
-  //         }
-  //       }
 
-  //     })
-  //     .map((animal) => (
-  //       <Card
-  //         type={animal.animal_type}
-  //         name={animal.name}
-  //         image={animal.image_link}
-  //         latinName={animal.latin_name}
-  //         lifeSpan={animal.lifespan}
-  //         habitat={animal.habitat}
-  //         diet={animal.diet}
-  //         activeTime={animal.active_time}
-  //       />
-  //     ));
-
-  //const ageGroups = [lifeSpansBelow11]
-
-  // animal =>
-  // do I have a filter set? no, return true (give me back every item)
-  // otherwise, should I return this item?
-  // animaltype == myfilter? true (gives you it back), false (filters it out)
   const filteredCards = allResults
-    // in your filter:
-    // if selectedLifespan === "1-20":
-    // if animal.lifespan < 20 return true
-    // if selectedLifespan === "21-30"
-    // if animal.lifespan > 20 && animal.lifespan < 30 return true
+
     .filter((animal) => {
       if (selectedLifeSpan === "") {
         return true;
       }
       if (selectedLifeSpan === "1-20 years") {
-        if (animal.lifespan < 20) {
-          return true;
-        }
+        return animal.lifespan < 20;
       }
       if (selectedLifeSpan === "21-30 years") {
-        if (animal.lifespan > 20 && animal.lifespan < 30) {
-          return true;
-        }
+        return animal.lifespan > 20 && animal.lifespan < 30;
       }
       if (selectedLifeSpan === "31-50 years") {
-        if (animal.lifespan > 30) {
-          return true;
-        }
+        return animal.lifespan > 30;
       }
+      return false;
     })
     .filter((animal) => {
       if (selectedType === "") {
@@ -193,10 +112,6 @@ function App() {
         activeTime={animal.active_time}
       />
     ));
-
-  //filter for first carousel
-
-  //filters for lower carousel
 
   const isFilterPresent = selectedType !== "" || selectedActiveTime !== "";
   const filtersStrings = [selectedType, selectedActiveTime]
@@ -251,15 +166,6 @@ function App() {
           {!isFilterPresent ? "Choose a Filter" : `Filters: ${filtersStrings}`}
         </h3>
         <Carousel>{!isFilterPresent ? defaultCards : filteredCards}</Carousel>
-
-        {/* <div>
-					<h3>{title}</h3>
-					<CardContainer>
-						{products.map(() => (
-							<Card />
-						))}
-					</CardContainer>
-				</div> */}
       </div>
     </div>
   );
